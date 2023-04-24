a8cff02639dc46f55c7f2bdd5c2041902d24446c
ahmedhossam144 <ahmedhossamfekry9@gmail.com>
2023-04-24 11:42:57 -0700

    changes

--- get_flags.c
+++ get_flags.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
+ * get_flags - Calculates flags
+ * @format: Formatted string to print the arguments
+ * @i: take parameter.
  * Return: Flags:
  */
 int get_flags(const char *format, int *i)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
 	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
--- get_precision.c
+++ get_precision.c
@@ -2,9 +2,9 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @format: Formatted string to print the args
+ * @i: List of args printed.
+ * @list: list of args.
  *
  * Return: Precision.
  */
